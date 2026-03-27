f8a380eefa8370dbaf8caf9fc7839d24e8a0386c
Oleksii Chernenko <caseyjonesinthehouse@gmail.com>
2026-03-27 16:45:23 +0200

    wip: add loading spinner

--- ui/src/app/app.ts
+++ ui/src/app/app.ts
@@ -1,14 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 import { AppHeaderComponent } from './components/layout/app-header.component';
+import { LoadingService } from './services/loading.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppHeaderComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RouterOutlet, MatProgressBarModule, AppHeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
+  readonly isLoading = inject(LoadingService).isLoading;
   protected readonly title = signal('pb-banner-demo');
 }
--- ui/src/app/resolvers/banners.resolver.ts
+++ ui/src/app/resolvers/banners.resolver.ts
@@ -1,8 +1,24 @@
 import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
-import { BannerService } from '../services/banner.service';
+import { finalize, catchError } from 'rxjs';
+
 import { Banner } from '../models/banner.model';
+import { BannerService } from '../services/banner.service';
+import { LoadingService } from '../services/loading.service';
 
 export const bannersResolver: ResolveFn<Banner[]> = () => {
-  return inject(BannerService).getBanners();
+  const loadingService = inject(LoadingService);
+  loadingService.show();
+  return inject(BannerService)
+    .getBanners()
+    .pipe(
+      finalize(() => {
+        loadingService.hide();
+      }),
+      catchError((err) => {
+        console.error(err);
+        loadingService.hide();
+        throw err;
+      }),
+    );
 };
