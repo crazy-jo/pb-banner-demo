1698d9c89b43d091dd01c5d80654db4648d37d55
Oleksii Chernenko <caseyjonesinthehouse@gmail.com>
2026-03-27 15:33:20 +0200

    wip: enable cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
+  app.enableCors({
+    origin: 'http://localhost:4200', // Specific origin allowed (can be a string, array, or boolean)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allowed HTTP methods
+    credentials: true, // Allow the browser to send cookies and authorization headers
+  });
   console.log(`🚀 Running on: http://localhost:${port}`);
   await app.listen(port);
 }
